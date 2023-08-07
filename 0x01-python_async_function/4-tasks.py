0215e50259c704e7c719694fbbb5e6105af02809
nathan-assefa <aasamnew@gmail.com>
2023-08-07 13:07:23 +0000

    python_async_function

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -11,6 +11,7 @@ task_wait_random = __import__("3-tasks").task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """ returns delays """
     coroutines = [task_wait_random(max_delay) for _ in range(n)]
     delays = []
 
