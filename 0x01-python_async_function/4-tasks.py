3360302ab7e3a3d6a6af91d0ce6b524d16e62bbf
nathan-assefa <aasamnew@gmail.com>
2023-08-07 13:05:54 +0000

    python_async_function

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -9,9 +9,12 @@ from typing import List
 
 task_wait_random = __import__("3-tasks").task_wait_random
 
-
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """returning coroutine"""
     coroutines = [task_wait_random(max_delay) for _ in range(n)]
-    results = await asyncio.gather(*coroutines)
-    return results
+    delays = []
+
+    for completed_task in asyncio.as_completed(coroutines):
+        result = await completed_task
+        delays.append(result)
+
+    return delays
