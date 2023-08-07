34e7af92e4fa8ef19fc3e73ec7c115323ef0972b
nathan-assefa <aasamnew@gmail.com>
2023-08-07 10:11:35 +0000

    python_async_function

--- 0x01-python_async_function/try.py
+++ 0x01-python_async_function/try.py
@@ -13,10 +13,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ returning coroutine """
     coroutines = [wait_random(max_delay) for _ in range(n)]
-    delays = []
-
-    for coroutine in asyncio.as_completed(coroutines):
-        result = await coroutine
-        delays.append(result)
-
-    return delays
+    result = await asyncio.gather(*coroutines)
+    return result
