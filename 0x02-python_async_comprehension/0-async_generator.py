e8169ff3046b5902285765c24299c76d57a3f7e7
nathan-assefa <aasamnew@gmail.com>
2023-08-08 10:54:21 +0000

    python_async_comprehension

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,7 +4,7 @@ import random
 """ this script defines async generator """
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """ generating iterable """
     for _ in range(10):
         await asyncio.sleep(1)
