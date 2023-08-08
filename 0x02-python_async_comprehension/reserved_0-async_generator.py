ac3469208ac20e762c6133e08984665ebafcb858
nathan-assefa <aasamnew@gmail.com>
2023-08-08 12:39:12 +0000

    python_async_comprehension

--- 0x02-python_async_comprehension/reserved_0-async_generator.py
+++ 0x02-python_async_comprehension/reserved_0-async_generator.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-""" a python module to loop 10 times """
-import random
 import asyncio
+import random
 from typing import Generator
 
 
+""" this script defines async generator """
+
+
 async def async_generator() -> Generator[float, None, None]:
-    """
-    async_generator - function to loop 10 times
-    """
-    for i in range(10):
+    """ This functioin returns iterable """
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
