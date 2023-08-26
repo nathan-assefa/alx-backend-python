d77e830f134b0af17d4b1142f876521e82e5fa07
nathan-assefa <aasamnew@gmail.com>
2023-08-26 11:26:09 +0000

    integration testing

--- 0x03-Unittests_and_integration_tests/2-test_utils.py
+++ 0x03-Unittests_and_integration_tests/2-test_utils.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 """
-@parameterized.expand([
-    (input_arg1, input_arg2, ..., expected_output1),
-    (input_arg1, input_arg2, ..., expected_output2),
-    # ... more test cases
-])
+Testing utils script
 """
 import unittest
 from unittest.mock import patch, Mock
@@ -44,30 +40,6 @@ class TestAccessNestedMap(unittest.TestCase):
 class TestGetJson(unittest.TestCase):
     '''
     Testing a function that handles HTTP requests
-    ********* What is Mock? ***************
-    Mocking is primarily about replacing dependencies, not the methods
-    themselves. When you're using mocking, you're creating substitute
-    objects for the dependencies that a method relies on, allowing you to
-    control their ###behavior, responses, and interactions during testing.
-    This isolation of dependencies helps you focus solely on testing the
-    behavior of the method itself, without worrying about the correctness of
-    external systems or making actual network/database calls.
-
-    Example: When we say "mocking" requests.get, we mean that we're creating
-    a ----simulated version---- of the #requests.get function that behaves in
-    a ----controlled way----. This simulated version, created using the
-    unittest.mock library, allows us to define specific behaviors and return
-    values that we expect ####---- without actually executing the real
-    requests.get function and making actual HTTP calls ----####.
-
-
-    ********* What is Patch? **************
-    The patch context manager is a feature provided by the unittest.mock
-    module in Python. It's used to ----temporarily replace---- an object or
-    attribute with a mock object, enabling you to ##-----control and manipulate
-    the behavior of the object within a specific scope of your code.-----## The
-    patch context manager is particularly useful for -----isolating----- and
-    -----controlling----- the behavior of ----dependencies---- during testing.
     '''
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
@@ -77,32 +49,12 @@ class TestGetJson(unittest.TestCase):
     def test_get_json(self, test_url, test_payload, mock_requests_get):
         ''' Mocking(simulating) the requests.get() method '''
 
-        '''
-        **** How patch handles the replacment of requests.get? ****
-        The @patch decorator patches the utils.requests.get function, and it's
-        passed as the third parameter (mock_requests_get) to the test_get_json
-        method. This mock object is created and passed automatically by the
-        @patch decorator.
-        '''
-
         # Create a Mock object for the response from requests.get and
         # + configure its behavior
         mock_response = Mock()
 
         # Set the return value of the json method of the mock_response
-        '''
-        Configuring the behavior of the --json method-- of the mock_response
-        object. The --return_value-- attribute allows you to set what the
-        json method should return when it's called. In this case, you're
-        setting it to test_payload, which is the JSON data you want to
-        simulate as the response content.
-        '''
         mock_response.json.return_value = test_payload
-
-        '''
-        ********what is return_value?*********
-        The value returned when the mock is called.
-        '''
 
         # Set the return value of the whole mock_requests_get
         # + to be the mock_response
@@ -119,14 +71,7 @@ class TestGetJson(unittest.TestCase):
 
 
 class TestMemoize(unittest.TestCase):
-    '''
-    ****** what is memoization? ******
-    Memoization is an optimization technique that involves --storing--
-    the results of ##expensive function calls and returning the #cached
-    result when the same inputs occur again. This helps in avoiding
-    redundant computations and improving the performance of functions
-    that have heavy computations or repeated calculations.
-    '''
+    ''' Implementing memoization '''
     class TestClass:
 
         def a_method(self):
